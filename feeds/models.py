0ae5fa8cdab155bb6e390f8e6c4aaed020512e11
baali <baali@muse-amuse.in>
2016-08-22 13:12:08 +0530

    Added table for accounts of people to be polled for status update.

--- feeds/models.py
+++ feeds/models.py
@@ -8,3 +8,10 @@ class AuthTokens(models.Model):
     screen_name = models.CharField(max_length=60, unique=True)
     access_token = models.CharField(max_length=120)
     access_token_secret = models.CharField(max_length=120)
+
+class TwitterAccounts(models.Model):
+    screen_name = models.CharField(max_length=60, unique=True)
+    followed_from = models.ForeignKey(AuthTokens, on_delete=models.CASCADE)
+    last_updated = models.DateTimeField()
+    def __str__(self):              # __unicode__ on Python 2
+        return self.screen_name
