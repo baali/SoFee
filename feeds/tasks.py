a6f088e45dd277a0a02c1568b027478f60bcd4fc
baali <baali@muse-amuse.in>
2016-08-22 13:13:14 +0530

    Task to updated RSS files with status.

--- feeds/tasks.py
+++ feeds/tasks.py
@@ -11,8 +11,67 @@ import ConfigParser
 # Create your views here.
 config = ConfigParser.RawConfigParser()
 config.read('keys.cfg')
-from models import AuthTokens
+from models import AuthTokens, TwitterAccounts
 from django.contrib.staticfiles.templatetags.staticfiles import static
+
+@app.task(bind=True)
+def update_rss_task(self):
+    for account in TwitterAccounts.objects.all():
+        auth = tweepy.OAuthHandler(config.get('twitter', 'consumer_key'), config.get('twitter', 'consumer_secret'))
+        auth_token = account.followed_from
+        auth.set_access_token(auth_token.access_token, auth_token.access_token_secret)
+        try:
+            api = tweepy.API(auth, wait_on_rate_limit=True)
+        except tweepy.TweepError:
+            print 'Error! Failed to get access token for user %s.' %account.screen_name
+            continue
+        statuses = api.user_timeline(screen_name=account.screen_name)
+        name = friend_url = screen_name = None
+        for status in statuses:
+            if status.created_at < account.last_updated:
+                break
+            if status.author.screen_name == account.screen_name:
+                # skipping tweets where someone else is talking to friend
+                name = status.author.name
+                friend_url = status.author.url
+                screen_name = status.author.screen_name
+                break
+        # condition when user has not added anything to the timeline
+        # since we last parsed their timeline
+        if screen_name == None:
+            continue
+        fg = FeedGenerator(name)
+        fg.id(friend_url)
+        fg.description('Tweets by ' + name)
+        fg.title(screen_name)
+        fg.author( {'name':name} )
+        fg.link( href=friend_url, rel='alternate' )
+        fg.language('en')
+        # get 10 time line activities for the friend
+        count = 0
+        current_group = None
+        for status in statuses:
+            if not status.author.screen_name == account.screen_name:
+                # skipping tweets where someone else is talking to friend
+                continue
+            if status.created_at < account.last_updated:
+                break
+            screen_name = status.author.screen_name
+            text = status.text
+            created = status.created_at
+            url = 'https://twitter.com/'+screen_name+'/status/'+status.id_str
+            fe = fg.add_entry()
+            fe.id(url)
+            fe.author({'name':name})
+            fe.title(text)
+            fe.description(text)
+            fe.pubdate(pytz.utc.localize(status.created_at))
+            account.last_updated = status.created_at
+            count += 1
+        account.save()
+        with open('feeds/static/xml/feed-%s.xml'%account.screen_name, 'w') as feed:
+            feed.write(fg.rss_str())
+            print 'Done getting status for user: %s' %account.name
 
 @app.task(bind=True)
 def rss_task(self, friend_url, screen_name, name, friend_id, timeline):
@@ -92,8 +151,8 @@ def opml_task(self, token, verifier, host_uri):
                     'total': me.friends_count,
                 }
             self.update_state( state='PROGRESS', meta=meta )
-            timeline = friend.timeline()
-            rss_task.apply_async([friend.url, friend.screen_name, friend.name, friend.id_str, timeline])
+            # timeline = friend.timeline()
+            # rss_task.apply_async([friend.url, friend.screen_name, friend.name, friend.id_str, timeline])
             entry = SubElement(body, 'outline',
                                {'text':friend.name,
                                 'title':friend.name,
@@ -112,3 +171,8 @@ def opml_task(self, token, verifier, host_uri):
 based on people you follow. You can access it here
 %s Use this file with any feed
 reader of you choice.'''%(host_uri+static('opml/'+me.screen_name+'.opml')))
+    meta = {'info': 'Done compiling list, you can download it from here: %s'%(host_uri+static('opml/'+me.screen_name+'.opml')) ,
+            'count': count,
+            'total': me.friends_count,
+    }
+    self.update_state( state='PROGRESS', meta=meta )
