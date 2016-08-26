e736d9bf3cdb64c284c4587a6f8cfc3cc999e7c4
baali <baali@muse-amuse.in>
2016-08-26 11:08:54 +0530

    wsgi file for gunicorn.

---
+++ tweet_d_feed/django_wsgi.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from django.core.wsgi import get_wsgi_application
+
+APP_NAME = 'tweet_d_feed'
+os.environ['DJANGO_SETTINGS_MODULE'] = APP_NAME + '.settings'
+
+FILE_PATH = os.path.dirname(os.path.realpath(__file__))
+SITE_ROOT = os.path.join(FILE_PATH, "..")
+
+PYTHON_EGG_CACHE_PATH = os.path.join(SITE_ROOT, r'.eggs_cache')
+
+APPS_PATH = os.path.join(SITE_ROOT, r'/home/baali/Tweet_De_Feed')
+# LIB_PATH = os.path.join(SITE_ROOT, r'lib')
+
+sys.path.append(APPS_PATH)
+# sys.path.append(LIB_PATH)
+application = get_wsgi_application()
