82132ce9fb3121d9bee098b65596a824aaeb607c
baali <baali@muse-amuse.in>
2016-10-01 12:07:49 +0530

    Revamping models to store tweets and other details.

--- feeds/models.py
+++ feeds/models.py
@@ -4,14 +4,26 @@ from django.db import models
 
 # Create your models here.
 
-class AuthTokens(models.Model):
+class AuthToken(models.Model):
+    uuid = models.CharField(max_length=64, default=uuid.uuid4,
+                            db_index=True)
     screen_name = models.CharField(max_length=60, unique=True)
     access_token = models.CharField(max_length=120)
     access_token_secret = models.CharField(max_length=120)
 
-class TwitterAccounts(models.Model):
+class TwitterAccount(models.Model):
     screen_name = models.CharField(max_length=60, unique=True)
     followed_from = models.ForeignKey(AuthTokens, on_delete=models.CASCADE)
     last_updated = models.DateTimeField()
     def __str__(self):              # __unicode__ on Python 2
         return self.screen_name
+
+class TwitterStatus(models.Model):
+    tweet_from = models.ForeignKey(TwitterAccount, on_delete=models.CASCADE)
+    followed_from = models.ForeignKey(AuthToken, on_delete=models.CASCADE)
+    # Now links/urls are excluded from 140 character limit
+    status_text = models.CharField(max_length=240, unique=True)
+    status_created = models.DateTimeField()
+    status_seen = models.BooleanField(default=False)
+    def __str__(self):              # __unicode__ on Python 2
+        return self.tweet_from, self.status_text
