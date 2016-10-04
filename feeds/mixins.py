781f2257a248a9bbcb91f2618212f35e6345210e
baali <baali@muse-amuse.in>
2016-10-04 12:42:04 +0530

    Cleaned up mixins file.

--- feeds/mixins.py
+++ feeds/mixins.py
@@ -3,8 +3,8 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.contrib.sites.shortcuts import get_current_site
 
+
 class UUIDMixin(models.Model):
-    
     uuid = models.CharField(max_length=64, default=uuid.uuid4,
                             db_index=True, primary_key=True, editable=False)
 
@@ -22,6 +22,6 @@ class UUIDMixin(models.Model):
     def get_uuid_url(self, request=None):
         if not self.pk:
             return None
-        ct=ContentType.objects.get_for_model(self)
+        ct = ContentType.objects.get_for_model(self)
         return "http://{0}/{1}/{2}/".format(get_current_site(request).domain,
                                             ct.model, self.pk)
