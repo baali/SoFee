60d5973f63ad13e8735b3800d6f481bbd15d96eb
baali <baali@muse-amuse.in>
2016-10-04 12:20:10 +0530

    Updated models, migrations, using UUID mixin.

--- feeds/models.py
+++ feeds/models.py
@@ -1,36 +1,54 @@
 from __future__ import unicode_literals
 
 from django.db import models
-import uuid
+from django.core.validators import URLValidator
+from feeds.mixins import UUIDMixin
+# from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
 
 
-class AuthToken(models.Model):
-    uuid = models.CharField(max_length=64, default=uuid.uuid4,
-                            db_index=True)
+class AuthToken(UUIDMixin):
     screen_name = models.CharField(max_length=60, unique=True)
     # Should they be stored directly
     access_token = models.CharField(max_length=120)
     access_token_secret = models.CharField(max_length=120)
 
 
-class TwitterAccount(models.Model):
+class TwitterAccount(UUIDMixin):
     screen_name = models.CharField(max_length=60, unique=True)
     followed_from = models.ForeignKey(AuthToken, on_delete=models.CASCADE)
     last_updated = models.DateTimeField()
 
-    def __str__(self):              # __unicode__ on Python 2
+    def __str__(self):              
         return self.screen_name
 
 
-class TwitterStatus(models.Model):
+class TwitterStatus(UUIDMixin):
     tweet_from = models.ForeignKey(TwitterAccount, on_delete=models.CASCADE)
     followed_from = models.ForeignKey(AuthToken, on_delete=models.CASCADE)
     # Now links/urls are excluded from 140 character limit
-    status_text = models.CharField(max_length=240, unique=True)
+    status_text = models.CharField(max_length=240)
     status_created = models.DateTimeField()
     status_seen = models.BooleanField(default=False)
+    status_url = models.URLField()
+    # status_json = JSONField(default={})
 
-    def __str__(self):              # __unicode__ on Python 2
+    class Meta:
+        ordering = ('status_created',)
+
+    def __str__(self):
         return self.tweet_from, self.status_text
+
+class TwitterLink(UUIDMixin):
+    url = models.URLField(db_index=True)
+    shared_from = models.ForeignKey(TwitterAccount, on_delete=models.CASCADE)
+    url_shared = models.DateTimeField()
+    url_seen = models.BooleanField(default=False)
+    # tweet_json = JSONField(default={})
+
+    class Meta:
+        ordering = ('url_shared',)
+
+    def __str__(self):              
+        return self.shared_from, self.url
