5869cdffc94c545a9c4f8b3148bed712efca48b9
baali <baali@muse-amuse.in>
2016-10-13 02:16:29 +0530

    Updated management command for the latest changes in models.

--- feeds/management/commands/populate_users.py
+++ feeds/management/commands/populate_users.py
@@ -5,8 +5,7 @@ from django.core.management.base import BaseCommand, CommandError
 import tweepy
 import pytz
 
-from feeds.models import AuthToken, TwitterAccount
-from feeds.tasks import rss_task
+from feeds.models import AuthToken, TwitterAccount, TwitterStatus, UrlShared
 
 
 class Command(BaseCommand):
@@ -34,22 +33,48 @@ add them to TwitterAccounts table to be picked up by celery scheduler task.'''
         for friend in tweepy.Cursor(api.friends).items():
             if not friend.url:
                 continue
-            if TwitterAccount.objects.filter(screen_name=friend.screen_name).exists():
-                continue
             timeline = friend.timeline()
             statuses = [status for status in timeline if status.author.screen_name == friend.screen_name]
             if statuses:
                 last_updated = pytz.utc.localize(statuses[0].created_at)
             else:
                 last_updated = pytz.utc.localize(datetime.datetime.now())
-
-            twitter_account = TwitterAccount.objects.create(
-                screen_name=friend.screen_name,
-                followed_from=auth_token,
-                last_updated=last_updated
-            )
-            try:
+            twitter_account, created = TwitterAccount.objects.get_or_create(screen_name=friend.screen_name, defaults={'last_updated':last_updated})
+            if created:
                 twitter_account.save()
-            except:
-                raise CommandError('Issue with friend %s' % friend.screen_name)
-            rss_task.apply_async([friend.url, friend.screen_name, friend.name, friend.id_str, statuses])
+                twitter_account.followed_from.add(auth_token)
+            else:
+                if not twitter_account.followed_from.filter(uuid=auth_token.uuid).exists():
+                    twitter_account.followed_from.add(auth_token)
+                twitter_account.last_updated = last_updated
+            twitter_account.save()
+            for tweet in statuses:
+                if pytz.utc.localize(tweet.created_at) > twitter_account.last_updated:
+                    twitter_account.last_updated = pytz.utc.localize(tweet.created_at)
+                if getattr(tweet, 'retweeted_status', None) and tweet.text.endswith(u'\u2026'):
+                    text = twitter_account.screen_name + ' Retweeted ' + tweet.retweeted_status.author.screen_name + ': ' + tweet.retweeted_status.text
+                else:
+                    text = tweet.text
+                tweeted_at = pytz.utc.localize(tweet.created_at)
+                url = 'https://twitter.com/' + twitter_account.screen_name + '/status/' + tweet.id_str
+                status_obj = TwitterStatus(
+                    tweet_from=twitter_account,
+                    followed_from=auth_token,
+                    status_text=text,
+                    status_url=url)
+                status_obj.status_created=tweeted_at
+                status_obj.save()
+
+                if tweet._json['entities'].get('urls', []):
+                    for url_entity in tweet._json['entities']['urls']:
+                        if url_entity.get('expanded_url', ''):
+                            shared_at = pytz.utc.localize(tweet.created_at)
+                            link_obj, created = UrlShared.objects.get_or_create(
+                                url=url_entity['expanded_url'],
+                                defaults={'url_shared':pytz.utc.localize(tweet.created_at)})
+                            if created:
+                                link_obj.save()
+                            if not link_obj.shared_from.filter(uuid=twitter_account.uuid).exists():
+                                link_obj.shared_from.add(twitter_account)
+                            link_obj.save()
+            # rss_task.apply_async([friend.url, friend.screen_name, friend.name, friend.id_str, statuses])
