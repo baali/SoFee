a0e50e65e57df10078e4ef71d1eee6982e239f68
baali <baali@muse-amuse.in>
2017-02-06 16:57:06 +0530

    Updated URLs for latest API.

--- feeds/urls.py
+++ feeds/urls.py
@@ -4,6 +4,15 @@ from feeds import views
 status_list = views.StatusViewSet.as_view({
     'get': 'list',
 })
+url_listing = views.UrlViewSet.as_view({
+    'get': 'list',
+})
+get_feed = views.UrlViewSet.as_view({
+    'get': 'get_feed'
+})
+share_url = views.UrlViewSet.as_view({
+    'post': 'share_url'
+})
 
 urlpatterns = [
     url(
@@ -27,8 +36,11 @@ urlpatterns = [
         name='get_status',
     ),
     url(r'^index/(?P<uuid>[a-zA-Z0-9-]+)/$', views.index, name='index'),
-    url(r'^links/(?P<uuid>[a-zA-Z0-9-]+)/$', views.url_list, name='links'),
     url(r'^opml/(?P<uuid>[a-zA-Z0-9-]+)/$', views.opml, name='opml'),
     url(r'^status/(?P<uuid>[a-zA-Z0-9-]+)/$', status_list, name='statuses'),
-    url(r'^api-auth/(?P<uuid>[a-zA-Z0-9-]+)/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^urls/(?P<uuid>[a-zA-Z0-9-]+)/$', url_listing, name='urls'),
+    url(r'^urls/(?P<uuid>[a-zA-Z0-9-]+)/get_feed/$', get_feed, name='get_feed'),
+    url(r'^urls/(?P<uuid>[a-zA-Z0-9-]+)/share_url/$', share_url, name='share_url'),
+    url(r'^api-auth/(?P<uuid>[a-zA-Z0-9-]+)/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^(.*.js)$', views.sw_js, name='sw_js'),
 ]
