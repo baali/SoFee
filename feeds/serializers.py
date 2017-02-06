3503be5666c677d66127f236d46b9ef3b9592609
baali <baali@muse-amuse.in>
2017-02-06 16:50:46 +0530

    Serializer to limit JSON fields.

--- feeds/serializers.py
+++ feeds/serializers.py
@@ -2,10 +2,24 @@ from rest_framework import serializers
 from feeds.models import UrlShared, TwitterStatus, TwitterAccount
 
 
+class SmallerSetJsonField(serializers.JSONField):
+    """Class to expose Smaller set of JSON fields."""
+    def to_representation(self, value):
+        limited_dict = {}
+        if 'profile_image_url_https' in value:
+            limited_dict['profile_image_url'] = value['profile_image_url_https']
+        limited_dict['url'] = 'https://twitter.com/' + value.get('screen_name', '')
+        limited_dict['screen_name'] = value.get('screen_name', '')
+        limited_dict['name'] = value.get('name', '')
+        return limited_dict
+
+
 class TwitterAccountSerializer(serializers.ModelSerializer):
+    account_json = SmallerSetJsonField()
+
     class Meta:
         model = TwitterAccount
-        fields = ('screen_name', )
+        fields = ('screen_name', 'account_json')
 
 
 class UrlSerializer(serializers.ModelSerializer):
@@ -13,7 +27,7 @@ class UrlSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UrlShared
-        fields = ('uuid', 'url', 'shared_from', 'url_shared', 'url_seen', 'quoted_text', 'cleaned_text')
+        fields = ('uuid', 'url', 'shared_from', 'url_shared', 'url_seen', 'quoted_text', 'cleaned_text', 'url_json')
 
 
 class StatusSerializer(serializers.ModelSerializer):
