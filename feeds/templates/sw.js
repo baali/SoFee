9d5606e2e4f107385c3dbd0ab5a0008dd717c1ba
baali <baali@muse-amuse.in>
2017-02-22 17:02:52 +0530

    BugFix: To handle fresh fetch requests.

--- feeds/templates/sw.js
+++ feeds/templates/sw.js
@@ -33,9 +33,13 @@ self.addEventListener('fetch', function(event) {
     event.respondWith(
       caches.match(event.request).then(function(response) {
         return response || fetch(event.request).then(function(response) {
+          if (!response.ok) {
+            throw Error(response.statusText);
+          }
           caches.open(CACHE).then(function(cache) {
             return cache.put(event.request, response);
           });
+          return response;
         });
       })
     );
