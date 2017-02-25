9857396e5580fdf70ad986be1170299e2e324d71
baali <baali@muse-amuse.in>
2017-02-25 11:11:50 +0530

    First working version of Infinite scroll.

--- feeds/static/js/status.js
+++ feeds/static/js/status.js
@@ -1,5 +1,19 @@
 var link_details = {}
+var next_page = '';
 
+// Based on SO Conversation http://stackoverflow.com/questions/10072216/jquery-infinite-scroll-with-div-not-scrollbar-of-body
+// scrollFire option to update the last li element didn't work for dynamic elements
+// var options = [{selector: '.collection-item.last-li', offset: 50, callback: function(el) {
+//   update_feed(next_page);
+// }},]
+// Materialize.scrollFire(options);
+
+$(window).scroll(function() {
+  if($(window).scrollTop() + $(window).height() == $(document).height()) {
+    update_feed(next_page);
+  }
+});
+ 
 const messaging = firebase.messaging();
 $( document ).ready(function(){
   $(".button-collapse").sideNav();
@@ -73,27 +87,60 @@ function update_feed(url) {
     return
   }
   console.log('Updating feed...');
-  $.get(url)
-    .done( function(data) {
-      $('#tweets').empty();
-      next_page = data['next'];
-      $.each(data['results'], function(index, obj) {
-        $('#tweets').append(
-          $('<li class="collection-item">').append(
-            $('<span>').text(obj.tweet_from['screen_name']+' : '+obj.status_text)
-          )
-        );
+  fetch(next_page).then(function(response) {
+    response.json().then(function(data) {
+      next_page = data.next;
+      data.results.forEach(function(obj, index, array) {
+        var shared_from = "";
+        if (obj.shared_from !== undefined) {
+          var quoted_text = '';
+          quoted_text = obj.quoted_text;
+          if ('title' in obj.url_json && obj.url_json.title.length > 0) {
+            quoted_text += '<br/><strong>Title</strong>: '+obj.url_json.title;
+          }
+          $.each(obj.shared_from, function(index_names, name) {
+            if ('profile_image_url' in name.account_json) {
+              shared_from =
+                $('<div class="chip">').append(
+                  $('<a>').attr('href', name.account_json.url).append(
+                    $('<img class="circle responsive-img">').attr('src', name.account_json.profile_image_url)).attr(
+                      'target', '_blank').append(name.account_json.screen_name));
+            }
+            else {
+              shared_from += name.screen_name+' ';
+            }
+          });
+          $('#tweets').append(
+            $('<li class="collection-item">').append(
+              $('<div>').append(shared_from).append(
+                $('<span>').append(quoted_text+' ').append(
+                  $('<a>').attr('href', obj.url).attr(
+                    'target', '_blank').append(
+                      $('<i class="tiny material-icons">').text('send'))))).attr(
+                        'li-uuid', obj.uuid));
+        }
+        else {
+          if ('profile_image_url' in obj.tweet_from.account_json) {
+            shared_from =
+              $('<div class="chip">').append(
+                $('<a>').attr('href', obj.tweet_from.account_json.url).append(
+                  $('<img class="circle responsive-img">').attr('src', obj.tweet_from.account_json.profile_image_url)).attr(
+                    'target', '_blank').append(obj.tweet_from.account_json.screen_name));
+          }
+          else {
+            shared_from += obj.tweet_from.screen_name+' ';
+          }
+          $('#tweets').append(
+            $('<li class="collection-item">').append(
+              $('<div>').append(shared_from).append(
+                $('<span>').text(obj.status_text)
+              )
+            )
+          );
+        }
       });
-      var last_li = $('li').last();
-      last_li.attr('id', 'last_li');
-      var options = [
-        {selector: '#last_li', offset: 190, callback: function(el) {
-          // Materialize.toast("This is our ScrollFire Demo!", 1500 );
-          update_feed(next_page);
-        }},
-      ]
-      Materialize.scrollFire(options);
-    })
+    });
+  });
 }
 
 function get_status(uuid) {
@@ -128,15 +175,6 @@ function get_status(uuid) {
           )
         );
       });
-      var last_li = $('li').last();
-      last_li.attr('id', 'last_li');
-      var options = [
-        {selector: '#last_li', offset: 50, callback: function(el) {
-          // Materialize.toast("This is our ScrollFire Demo!", 1500 );
-          update_feed(next_page);
-        }},
-      ]
-      Materialize.scrollFire(options);
     });
   });
 }
@@ -151,6 +189,7 @@ function get_links(uuid) {
     navigator.serviceWorker.onmessage = function(event) {
       console.log('Service worker returned a message!');
       link_details = {};
+      next_page = event.data.next;
       $.each(event.data.results, function(index, obj) {
         var shared_from = "";
         $.each(obj.shared_from, function(index_names, name) {
@@ -194,6 +233,7 @@ function get_links(uuid) {
     console.log('Making our own JS Request!');
     $.get("/urls/"+uuid+"/")
       .done( function(data) {
+        next_page = data.next;
         link_details = {};
         $.each(data.results, function(index, obj) {
           var shared_from = "";
